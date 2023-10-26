2e5245941b51301ce1c3f837ff0e2d46ae75bf5b
dejvid dadic <dejvid.dadic@docc.techstarter.de>
2023-10-26 16:07:19 +0200

    aufgabe 4

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
@@ -12,3 +12,16 @@ skills = ["AWS", "Python", "Git"]
 @app.get("/", response_class=HTMLResponse)
 async def get_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "name": name, "skills": skills})
+
+@app.get("/contact", response_class=HTMLResponse)
+def read_contact(request: Request):
+    return templates.TemplateResponse("contact.html", {"request": request, "name": name})
+
+@app.post("/contact", response_class=HTMLResponse)
+def submit_contact(
+    request: Request,
+    name: str = Form(...),
+    email: str = Form(...),
+    message: str = Form(...)
+):
+    return templates.TemplateResponse("contact_submission.html", {"request": request, "name": name, "email": email, "message": message})
